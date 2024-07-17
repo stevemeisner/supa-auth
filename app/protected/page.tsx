9b0eef57c11350f0ece29ebfcde925db3e74f532
Steve Meisner <steve.meisner@hey.com>
2024-07-16 20:59:21 -0400

    chore: adds prettier and formats project
    chore: updates packages with `pnpm up`
    
    chore: added prettier and formatted files
    
    chore: added a asdf `.tool-versions` file
    
    chore: moves ts to dev deps

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,19 +1,19 @@
-import DeployButton from "@/components/DeployButton";
-import AuthButton from "@/components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
-import Header from "@/components/Header";
-import { redirect } from "next/navigation";
+import DeployButton from '@/components/DeployButton'
+import AuthButton from '@/components/AuthButton'
+import { createClient } from '@/utils/supabase/server'
+import FetchDataSteps from '@/components/tutorial/FetchDataSteps'
+import Header from '@/components/Header'
+import { redirect } from 'next/navigation'
 
 export default async function ProtectedPage() {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const {
     data: { user },
-  } = await supabase.auth.getUser();
+  } = await supabase.auth.getUser()
 
   if (!user) {
-    return redirect("/login");
+    return redirect('/login')
   }
 
   return (
@@ -41,7 +41,7 @@ export default async function ProtectedPage() {
 
       <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
         <p>
-          Powered by{" "}
+          Powered by{' '}
           <a
             href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
             target="_blank"
@@ -53,5 +53,5 @@ export default async function ProtectedPage() {
         </p>
       </footer>
     </div>
-  );
+  )
 }
